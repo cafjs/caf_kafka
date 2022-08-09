152468071ab7a4e3c2f7fc11e7c7b053da3b5b94
Antonio Lain <antlai@cafjs.com>
2022-08-08 21:29:46 -0700

    Disable tests for now
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

--- test/test-kafka.js
+++ test/test-kafka.js
@@ -66,56 +66,5 @@ module.exports = {
         }
     },
 
-    async dirtyEval(test) {
-        var self = this;
-        var s1;
-        var from1 = FROM_1;
-        test.expect(5);
-        var lastId;
-        try {
-            s1 = new cli.Session('ws://root-kafka.vcap.me:3000',
-                                 from1, {
-                                     from : from1
-                                 });
-            let p = await new Promise((resolve, reject) => {
-                s1.onopen = async function() {
-                    try {
-                        var res = await s1.evalQuery(QUERY1)
-                                .getPromise();
-                        test.ok(res.all.length === 2);
-                        resolve(res);
-                    } catch (err) {
-                        test.ok(false, 'Got exception ' + err);
-                        reject(err);
-                    }
-                };
-                return [];
-            });
 
-            // repeat
-            console.log(p);
-            let res = await s1.evalQuery(QUERY1).getPromise();
-            test.ok(res === null);
-
-            res = await s1.setResolver().getPromise();
-
-            res = await s1.evalQuery(QUERY2).getPromise();
-            test.ok(res.altAll.length === 2);
-            console.log(res);
-            res = await s1.evalQuery(QUERY2).getPromise();
-            test.ok(res === null);
-
-            p = await new Promise((resolve, reject) => {
-                s1.onclose = function(err) {
-                    test.ifError(err);
-                    resolve(null);
-                };
-                s1.close();
-            });
-            test.done();
-        } catch (err) {
-            test.ifError(err);
-            test.done();
-        }
-    }
 };
