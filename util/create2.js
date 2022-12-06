10e7b8b1ff922a829c389fad9b9485cacd1ea841
Antonio Lain <antlai@cafjs.com>
2022-12-05 20:36:44 -0800

    Add util
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

---
+++ util/create2.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { Kafka } = require('kafkajs');
+
+const init = async function() {
+    const username = process.env.KAFKA_USERNAME;
+    const password = process.env.KAFKA_PASSWORD;
+
+    const kafka = new Kafka({
+        clientId: 'my-test-app',
+        brokers: ['pkc-lgk0v.us-west1.gcp.confluent.cloud:9092'],
+        ssl: true,
+        sasl: {
+            mechanism: 'plain',
+            username,
+            password
+        }
+    });
+
+    const producer = kafka.producer();
+
+    await producer.connect();
+    const messages = [];
+
+    for (let i=0; i<100; i++) {
+        const source = `user${i}`;
+        for (let j=0; j<100; j++) {
+            const dest = Math.floor(Math.random()*100);
+            const target = `user${dest}`;
+            messages.push({
+                key: source,
+                value: JSON.stringify({change: -1})
+            });
+            messages.push({
+                key: target,
+                value: JSON.stringify({change: 1})
+            });
+        }
+    }
+
+    await producer.send({
+        topic: 'hellokafka-10k',
+        messages
+    });
+
+    await producer.disconnect();
+};
+
+init();
